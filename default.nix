3c00937c31f5d0c592c6719ab7499220f13a09c5
Faure Adrien <adrien.faure2@gmail.com>
2021-10-14 15:41:31 +0200

    ci: fix default and artifact path

--- default.nix
+++ default.nix
@@ -1,23 +1,61 @@
-{ pkgs ? (import <nixpkgs> {}).pkgs }:
-{
-
-  sequence-diagram = pkgs.stdenv.mkDerivation rec {
-    name = "sequence-diagram";
+{ pkgs ? (import <nixpkgs> { }).pkgs }:
+let
+  nodeDependencies = (pkgs.callPackage ({ pkgs, system }:
+    let nodePackages = import ./release.nix { inherit pkgs system; };
+    in nodePackages // {
+      shell = nodePackages.shell.override {
+        buildInputs = [
+          # pkgs.nodePackages.node-gyp-build
+        ];
+      };
+    }) { }).shell.nodeDependencies;
+in {
+  melomelon = pkgs.stdenv.mkDerivation rec {
+    name = "melomelon";
     src = pkgs.lib.sourceByRegex ./. [
-      "^docs"
-      "^docs/seq.txt"
+      "^src"
+      # Chrome manifest
+      "^src/manifest.json"
+      # Style files
+      "^src/css"
+      "^src/css/.*.scss"
+      # Html
+      "^src/html"
+      "^src/html/.*.html"
+      # Html
+      "^src/images"
+      "^src/images/.*.png"
+      # Javascript
+      "^src/js"
+      "^src/js/.*.js"
+      # Vue templates
+      "^src/templates"
+      "^src/templates/.*.vue"
+      # Webpack config
+      "^webpack.config.js"
+      "^webpack.prod.js"
+      # npm
+      "^package.json"
+      "^package-lock.json"
     ];
-    buildInputs = [ pkgs.plantuml ];
-    buildPhase = "(cd docs && plantuml seq.txt)";
+    buildInputs = [ pkgs.nodejs pkgs.nodePackages.npm pkgs.nodePackages.webpack pkgs.nodePackages.webpack-cli ];
+
+    buildPhase = ''
+      ln -s ${nodeDependencies}/lib/node_modules ./node_modules
+
+      export PATH="${nodeDependencies}/bin:$PATH"
+
+      # Build the distribution bundle in "dist"
+      npx webpack-cli -c webpack.prod.js
+    '';
+
     installPhase = ''
       mkdir -p $out
-      mv docs/* $out/
+      mv build/* $out/
     '';
   };
 
   shell = pkgs.mkShell {
-    buildInputs = [
-      pkgs.nodePackages.npm
-    ];
+    buildInputs = [ pkgs.nodePackages.npm pkgs.nodePackages.node2nix pkgs.nodePackages.webpack ];
   };
 }
